45ba0562484dede352e3e55fed647875d6d08369
kapokris <krishnakap2222@gmail.com>
2025-08-23 19:04:51 -0400

    fixed warnings on console

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 function connectToDb() {
+    console.log("connectToDb function called");
   mongoose.connect(
-    process.env.DB_CONNECT,
-    { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    process.env.DB_CONNECT).then(()=>{
         console.log("connected to DB")
     }).catch(err=>console.log(err));
 }
